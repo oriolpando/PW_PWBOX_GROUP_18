c8802d540e9cd7e7220029cde9e834f1209dcab1
oriolpando <oriolpando@gmail.com>
2018-04-19 21:13:18 +0200

    Date limited and showing correct divs on click

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -1,17 +1,44 @@
-var i = 0;
+
 
 
 function showLogin() {
     console.log("fuck2");
+    var login = document.getElementById("login");
+    console.log(login);
+    login.style.display = "block";
+    var register = document.getElementById("register");
+    register.style.display = "none";
 }
 function showRegister() {
-    console.log("fuck2");
+    var login = document.getElementById("login");
+    login.style.display = "none";
+    var register = document.getElementById("register");
+    register.style.display = "block";
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+
+    today = yyyy+'-'+mm+'-'+dd;
+    var date = document.getElementById("birth").setAttribute("max", today   );
 }
 
 function controlLogin() {
-    console.log("fuck3");
+    var login = document.getElementById("login");
+    login.style.display = "block";
+    var register = document.getElementById("register");
+    register.style.display = "none";
 }
 
 function controlRegister() {
-    console.log("fuck4");
+    var login = document.getElementById("login");
+    login.style.display = "block";
+    var register = document.getElementById("register");
+    register.style.display = "none";
 }
