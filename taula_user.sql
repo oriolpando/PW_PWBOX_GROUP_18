b793ec1b6992e330c8381ec5de9889c87f8f7abf
miquelator <miquelet-sans@hotmail.com>
2018-05-03 14:02:02 +0200

    Fitxer taula de user

---
+++ taula_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) NOT NULL,
+  `surname` varchar(40) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `birth_date` date NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `pswUser` varchar(12) NOT NULL,
+  `image` varchar (50),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
