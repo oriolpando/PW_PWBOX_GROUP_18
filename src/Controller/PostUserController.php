26e015e827de3face5bf396abdc510f29bea3063
miquelator <miquelet-sans@hotmail.com>
2018-05-07 11:45:08 +0200

    comprovacions de registre i comencament de guardar imatges/crear carpetes

--- src/Controller/PostUserController.php
+++ src/Controller/PostUserController.php
@@ -72,7 +72,6 @@ class PostUserController
         $target_dir = "assets/resources/imatges/perfils";
 
         if(!empty($target_dir)){
-           echo("Not empty");
 
 
            if($_FILES["image"]["size"]>500000){
@@ -94,6 +93,7 @@ class PostUserController
            }
 
        }
+
 
 
         if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$date)) {
